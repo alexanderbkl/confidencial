56c1c63f13dac58d8670972acd7e5f33f7454eaf
Alexander <asederado@gmail.com>
2023-11-06 02:01:11 +0100

    add first menu buttons and game scene

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void OpenOptions()
+    {
+        // Options logic goes here
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
